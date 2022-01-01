73274bec8d313282bcf187af52ed27a94d687b07
wnima <zmxzmz@msn.cn>
2022-01-01 20:23:52 +0800

    添加Probuf

---
+++ small_protoc-3.7.1/protoc-3.7.1/1.bat
@@ -0,0 +1,22 @@
+@echo off
+
+set DIR=%~dp0
+set INPUTDIR=proto
+set JAVADIR=java
+echo "%DIR%"
+
+cd /d %DIR%%INPUTDIR%
+
+setlocal enabledelayedexpansion
+
+if exist %DIR%%JAVADIR% rmdir /s /q %DIR%%JAVADIR%
+@echo "mkdir java"
+mkdir %DIR%%JAVADIR%
+
+for /r %%i in (*.proto) do (
+	%DIR%protoc --proto_path=%DIR%%INPUTDIR% --java_out=%DIR%%JAVADIR% %%i
+)
+
+echo "finished"
+cd /d %DIR%
+pause
---
+++ small_protoc-3.7.1/protoc-3.7.1/ReadMe.txt
@@ -0,0 +1,5 @@
+客户端使用protobuf
+1. 拷贝文件
+proto中的文件拷贝到 resources 目录下的proto中。
+2.生成可用typeScript文件
+点击Cocos creator 菜单栏的 "扩展"按钮,弹出面板后,选择其中的 proto-kill 按钮即完成
